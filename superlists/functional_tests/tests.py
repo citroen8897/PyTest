f6cf8814bd40938c72050cdc93b7f20bfc6409af
predator2508 <citroen8897@gmail.com>
2021-10-21 21:36:51 +0300

    update 21_10_2021_fin

---
+++ superlists/functional_tests/tests.py
@@ -0,0 +1,50 @@
+from django.test import LiveServerTestCase
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+import time
+from selenium.common.exceptions import WebDriverException
+
+MAX_WAIT = 10
+
+
+class NewVisitorTest(LiveServerTestCase):
+    def setUp(self):
+        self.browser = webdriver.Firefox()
+
+    def tearDown(self):
+        self.browser.quit()
+
+    def wait_for_row_in_list_table(self, row_text):
+        start_time = time.time()
+        while True:
+            try:
+                table = self.browser.find_element_by_id('id_list_table')
+                rows = table.find_elements_by_tag_name('tr')
+                self.assertIn(row_text, [row.text for row in rows])
+                return
+            except (AssertionError, WebDriverException) as e:
+                if time.time() - start_time > MAX_WAIT:
+                    raise e
+                time.sleep(0.5)
+
+    def test_can_start_a_list_and_retrive_it_later(self):
+        self.browser.get(self.live_server_url)
+
+        self.assertIn('To-Do', self.browser.title)
+        header_text = self.browser.find_element_by_tag_name('h1').text
+        self.assertIn('To-Do', header_text)
+
+        inputbox = self.browser.find_element_by_id('id_new_item')
+        self.assertEqual(inputbox.get_attribute('placeholder'), 'Введите задачу')
+        inputbox.send_keys('Купить летнюю резину')
+        inputbox.send_keys(Keys.ENTER)
+        self.wait_for_row_in_list_table('1: Купить летнюю резину')
+
+        inputbox = self.browser.find_element_by_id('id_new_item')
+        inputbox.send_keys('Перебортировать летние колеса')
+        inputbox.send_keys(Keys.ENTER)
+
+        self.wait_for_row_in_list_table('1: Купить летнюю резину')
+        self.wait_for_row_in_list_table('2: Перебортировать летние колеса')
+
+        self.fail('Закончить тест!')
