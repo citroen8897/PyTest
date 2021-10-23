a2d677f076b2245db1c3286186dfc8fcd0352ac6
predator2508 <citroen8897@gmail.com>
2021-10-23 11:45:24 +0300

    update 23_10_2021

--- superlists/functional_tests/tests.py
+++ superlists/functional_tests/tests.py
@@ -48,3 +48,38 @@ class NewVisitorTest(LiveServerTestCase):
         self.wait_for_row_in_list_table('2: Перебортировать летние колеса')
 
         self.fail('Закончить тест!')
+
+    def test_start_a_list_for_one_user(self):
+        self.wait_for_row_in_list_table('1: Купить летнюю резину')
+        self.wait_for_row_in_list_table('2: Перебортировать летние колеса')
+
+    def test_multiple_users_can_start_lists_at_different_urls(self):
+        self.browser.get(self.live_server_url)
+        inputbox = self.browser.find_element_by_id('id_new_item')
+        inputbox.send_keys('Купить летнюю резину')
+        inputbox.send_keys(Keys.ENTER)
+        self.wait_for_row_in_list_table('1: Купить летнюю резину')
+
+        edith_list_url = self.browser.current_url
+        self.assertRegex(edith_list_url, '/lists/.+')
+
+        self.browser.quit()
+        self.browser = webdriver.Firefox()
+
+        self.browser.get(self.live_server_url)
+        page_text = self.browser.find_element_by_tag_name('body').text
+        self.assertNotIn('Купить летнюю резину', page_text)
+        self.assertNotIn('Перебортировать летние колеса', page_text)
+
+        inputbox = self.browser.find_element_by_id('id_new_item')
+        inputbox.send_keys('Купить продукты')
+        inputbox.send_keys(Keys.ENTER)
+        self.wait_for_row_in_list_table('1: Купить продукты')
+
+        francis_list_url = self.browser.current_url
+        self.assertRegex(francis_list_url, '/lists/.+')
+        self.assertNotEqual(francis_list_url, edith_list_url)
+
+        page_text = self.browser.find_element_by_tag_name('body').text
+        self.assertNotIn('Купить летнюю резину', page_text)
+        self.assertIn('Купить продукты', page_text)
